e3b08cd2b8deee91c0783631c4b75c2b251dd9d3
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-20 09:59:04 +0530

    new updates

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,13 @@ dotenv.config();
 
 const app = express();
 
-// Connect Database
 connectDB();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use("/api/auth", require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
