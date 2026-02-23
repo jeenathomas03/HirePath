ce021c090e5cdbba8c37f43794285b8b37dc2f56
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-23 05:50:11 +0530

    updates

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 HirePath | All Rights Reserved</p>
+      <p>Find jobs. Build resumes. Grow your career.</p>
+    </footer>
+  );
+}
+
+export default Footer;
