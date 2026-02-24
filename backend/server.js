dc2ce5d09e6baa90ad04ef164232be388e1278ab
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-24 11:04:36 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
+const app = express();
+
+// middleware
+app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/hirepath")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
+
+// routes
 const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
-app.use("/api", authRoutes);
-
+// server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
