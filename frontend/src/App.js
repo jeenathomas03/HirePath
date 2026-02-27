389ce68d028dc77884cafcdb216dbf828468bbb1
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-27 11:59:29 +0530

    new updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import RecruiterRegister from "./pages/LogiRecruiterRegistern";
+import RecruiterLogin from "./pages/RecruiterLogin";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/reg" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Recruiterregister" element={<RecruiterRegister />} />
+        <Route path="/Recruiterlogin" element={<RecruiterLogin />} />
         <Route path="/home"  element={<ProtectedRoute><Home /></ProtectedRoute> }/>
         <Route path="/profile"  element={<ProtectedRoute><Profile /></ProtectedRoute> }/>
       </Routes>
