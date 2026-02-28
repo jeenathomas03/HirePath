284b95a841f358ed94bcea306971d6585e2ab163
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-28 12:00:04 +0530

    new updates

--- backend/models/Recruiter.js
+++ backend/models/Recruiter.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
 const recruiterSchema = new mongoose.Schema({
-  name: String,
-  company: String,
-  email: {
-    type: String,
-    unique: true,
-  },
+  hrName: String,
+  companyName: String,
+  email: String,
+  phone: String,
+  companyRegNumber: String,
+  website: String,
+  address: String,
   password: String,
+  status: {
+    type: String,
+    default: "pending"
+  }
 });
 
 module.exports = mongoose.model("Recruiter", recruiterSchema);
