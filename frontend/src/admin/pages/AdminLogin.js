9b266ed863851dffed1e60bea15c8f7b30ccc885
jeenathomas03 <jeenathomas53@gmail.com>
2026-03-06 10:11:53 +0530

    new updates

---
+++ frontend/src/admin/pages/AdminLogin.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../styles/AdminLogin.css";
+
+function AdminLogin() {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/admin/login",
+        formData
+      );
+
+      localStorage.setItem("adminToken", res.data.token);
+      localStorage.setItem("admin", JSON.stringify(res.data.admin));
+
+      alert("Admin Login Successful");
+
+      navigate("/admin/dashboard");
+
+    } catch (error) {
+      alert("Invalid Admin Credentials");
+    }
+  };
+
+  return (
+    <div className="admin-login-container">
+
+      <div className="admin-login-box">
+        <h2>Admin Login</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Admin Email"
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit">Login</button>
+
+        </form>
+      </div>
+
+    </div>
+  );
+}
+
+export default AdminLogin;
